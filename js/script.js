25f26740b960bcbfaa72d85ddc2fd8f612441b93
zohairqureshi3 <zohair.qureshi3@gmail.com>
2023-01-16 22:38:58 +0500

    design

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var button = document.getElementById('login-button-img'); // Assumes element with id='button'
+
+button.onclick = function() {
+    var div = document.getElementById('newpost');
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
+    }
+    else {
+        div.style.display = 'block';
+    }
+};
